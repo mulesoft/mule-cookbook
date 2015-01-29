ac76e0dbac000fd81d6cd6cb06d2ed094c3463ef
pablocabrera85 <pablo.cabrera@mulesoft.com>
2015-01-29 15:27:40 -0300

    Add more business rules to server and tests to validate them

--- v1/model/src/main/java/org/mule/tools/devkit/store/model/Entity.java
+++ v1/model/src/main/java/org/mule/tools/devkit/store/model/Entity.java
@@ -8,13 +8,13 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 @XmlSeeAlso({ Author.class, Book.class })
 public abstract class Entity {
 
-    private int id;
+    private Integer id;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 }
