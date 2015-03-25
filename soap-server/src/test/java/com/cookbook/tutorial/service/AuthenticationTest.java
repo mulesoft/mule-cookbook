e0cdd1c4d7dd81205f7bd2420378462917dd22ba
pablocabrera85 <pablo.cabrera@mulesoft.com>
2015-03-24 17:55:16 -0700

    Remove duplicated 'the'

---
+++ soap-server/src/test/java/com/cookbook/tutorial/service/AuthenticationTest.java
@@ -0,0 +1,33 @@
+package com.cookbook.tutorial.service;
+
+import com.cookbook.tutorial.internal.service.CookBookDefaultBackEndImp;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotSame;
+
+/**
+ * Created by Mulesoft.
+ */
+public class AuthenticationTest {
+
+    MuleCookBookServiceImpl server;
+    String token;
+    @Before
+    public void setup() throws InvalidCredentialsException {
+
+        server = new MuleCookBookServiceImpl();
+        server.setServiceDAL(new CookBookDefaultBackEndImp());
+        token=server.login("admin","admin");
+    }
+
+    @Test(expected = InvalidTokenException.class)
+    public void invalidLogout() throws SessionExpiredException, InvalidEntityException, InvalidTokenException {
+        server.logout("ASDASD123");
+    }
+
+    @Test
+    public void validLoguot() throws SessionExpiredException, InvalidEntityException, InvalidTokenException {
+        server.logout(token);
+    }
+}
